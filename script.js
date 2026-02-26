7bb6704e3f89ea3746a518d4af123fa265befabe
ahadsazid <ahadsazid@gmail.com>
2026-02-26 21:03:24 +0600

    toggle task complete

--- script.js
+++ script.js
@@ -4,8 +4,44 @@ let rejectCount = document.getElementById("rejectCount");
 let sidebar = document.getElementById("sidebar");
 let allCards = document.getElementById("allCards");
 
+let interviewList = [];
+let rejectedList = [];
+
+const allBtn = document.getElementById("all-btn");
+const interBtn =document.getElementById("inter-btn");
+const rejectBtn = document.getElementById("reject-btn");
+
+const mainContainer = document.querySelector("main");
+
 function calculateCount(){
     total.innerText = allCards.children.length;
-    sidebar.innerText =allCards.children.length + "jobs";
+    sidebar.innerText = allCards.children.length + "jobs";
+    interviewCount.innerText = interviewList.length;
+    rejectCount.innerText = rejectedList.length;
+
 }
 calculateCount();
+
+function toggleStyle(id){
+    
+    allBtn.classList.remove("bg-blue-500", "text-white");
+    interBtn.classList.remove("bg-blue-500","text-white");
+    rejectBtn.classList.remove("bg-blue-500","text-white");
+
+    allBtn.classList.add("bg-white", "text-black");
+    interBtn.classList.add("bg-white","text-black");
+    rejectBtn.classList.add("bg-white","text-black");
+
+    
+
+    const selected =document.getElementById(id);
+
+    
+
+    selected.classList.remove("bg-white", "text-black");
+    selected.classList.add("bg-blue-500", "text-white");
+
+
+
+
+}
